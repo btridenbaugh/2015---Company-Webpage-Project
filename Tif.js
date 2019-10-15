6b0b42d6fd7430fa720460ab39ec4f17e90ac5c0
btridenbaugh <56405101+btridenbaugh@users.noreply.github.com>
2019-10-14 21:34:31 -0400

    Add files via upload

---
+++ Tif.js
@@ -0,0 +1,323 @@
+
+var a = 0;
+var b = 0;
+var c = 0;
+var d = 0;
+var e = 0;
+var f = 0;
+var g = 0;
+var h = 0;
+var i = 0;
+var j = 0;
+var k = 0;
+var l = 0;
+var m = 0;
+var n = 0;
+var o = 0;
+var p = 0;
+var q = 0;
+var r = 0;
+var s = 0;
+var t = 0;
+var u = 0;
+var v = 0;
+var w = 0;
+var x = 0;
+var y = 0;
+var z = 0;
+var tot = 0;
+
+function traditional_cupcakes_add() {
+a = a + 1;
+document.getElementById("traditional_cupcakes").innerHTML  = a;
+tot = tot + 18;
+}
+function traditional_cupcakes_minus() {
+a = a - 1;
+document.getElementById("traditional_cupcakes").innerHTML  = a;
+tot = tot - 18;
+}
+
+function decorated_cupcakes_add() { 
+b = b + 1;
+document.getElementById("decorated_cupcakes").innerHTML  = b;
+tot = tot + 18;
+}
+function decorated_cupcakes_minus() {
+b = b - 1;
+document.getElementById("decorated_cupcakes").innerHTML  = b;
+tot = tot - 18;
+}
+
+function gourmet_cupcakes_add() {
+c = c + 1;
+document.getElementById("gourmet_cupcakes").innerHTML  = c;
+tot = tot + 18;
+}
+function gourmet_cupcakes_minus() { 
+c = c - 1;
+document.getElementById("gourmet_cupcakes").innerHTML  = c;
+tot = tot - 18;
+}
+
+function original_cookies_add() {
+d = d + 1;
+document.getElementById("original_cookies").innerHTML  = d;
+tot = tot + 18;
+}
+function original_cookies_minus() {
+d = d - 1;
+document.getElementById("original_cookies").innerHTML  = d;
+tot = tot - 18;
+}
+
+function sugar_cookies_add() {
+e = e + 1;
+document.getElementById("sugar_cookies").innerHTML  = e;
+tot = tot + 18;
+}
+function sugar_cookies_minus() {
+e = e - 1;
+document.getElementById("sugar_cookies").innerHTML  = e;
+tot = tot - 18;
+}
+
+function small_tray_add() {
+f = f + 1;
+document.getElementById("small_tray").innerHTML  = f;
+tot = tot + 18;
+}
+function small_tray_minus() {
+f = f - 1;
+document.getElementById("small_tray").innerHTML  = f;
+tot = tot - 18;
+}
+
+function large_tray_add() {
+g = g + 1;
+document.getElementById("large_tray").innerHTML  = g;
+tot = tot + 18;
+}
+function large_tray_minus() { 
+g = g - 1;
+document.getElementById("large_tray").innerHTML  = g;
+tot = tot - 18;
+}
+
+function push_pops_add() {
+h = h + 1;
+document.getElementById("push_pops").innerHTML  = h;
+tot = tot + 18;
+}
+function push_pops_minus() {
+h = h - 1;
+document.getElementById("push_pops").innerHTML  = h;
+tot = tot - 18;
+}
+
+function cake_pops_add() {
+i = i + 1;
+document.getElementById("cake_pops").innerHTML  = i;
+tot = tot + 18;
+}
+function cake_pops_minus() {
+i = i - 1;
+document.getElementById("cake_pops").innerHTML  = i;
+tot = tot - 18;
+}
+
+function cupcake_cakes_12_add() {
+j = j + 1;
+document.getElementById("cupcake_cakes_12").innerHTML  = j;
+tot = tot + 18;
+}
+function cupcake_cakes_12_minus() {
+j = j - 1;
+document.getElementById("cupcake_cakes_12").innerHTML  = j;
+tot = tot - 18;
+}
+
+function cupcake_cakes_24_add() 
+k = k + 1;
+document.getElementById("cupcake_cakes_24").innerHTML  = k;
+tot = tot + 18;
+}
+function cupcake_cakes_24_minus() {
+k = k - 1;
+document.getElementById("cupcake_cakes_24").innerHTML  = k;
+tot = tot - 18;
+}
+ 
+function cupcake_cakes_48_add() { 
+l = l + 1;
+document.getElementById("cupcake_cakes_48").innerHTML  = l;
+tot = tot + 18;
+}
+function cupcake_cakes_48_minus() {
+l = l - 1;
+document.getElementById("cupcake_cakes_48").innerHTML  = l;
+tot = tot - 18;
+}
+
+function cakes_6_add() {
+m = m + 1;
+document.getElementById("cakes_6").innerHTML  = m;
+tot = tot + 18;
+}
+function cakes_6_minus() {
+m = m - 1;
+document.getElementById("cakes_6").innerHTML  = m;
+tot = tot - 18;
+}
+
+function cakes_8_add() {
+n = n + 1;
+document.getElementById("cakes_8").innerHTML  = n;
+tot = tot + 18;
+}
+function cakes_8_minus() {
+n = n - 1;
+document.getElementById("cakes_8").innerHTML  = n;
+tot = tot - 18;
+}
+
+function cakes_10_add() {
+o = o + 1;
+document.getElementById("cakes_10").innerHTML  = o;
+tot = tot + 18;
+}
+function cakes_10_minus() { 
+o = o - 1;
+document.getElementById("cakes_10").innerHTML  = o;
+tot = tot - 18;
+}
+
+function cakes_12_add() {
+p = p + 1;
+document.getElementById("cakes_12").innerHTML = p;
+tot = tot + 18;
+}
+function cakes_12_minus() {
+p = p - 1;
+document.getElementById("cakes_12").innerHTML  = p;
+tot = tot - 18;
+}
+
+function cakes_14_add() {
+q = q + 1;
+document.getElementById("cakes_14").innerHTML  = q;
+tot = tot + 18;
+}
+function cakes_14_minus() {
+q = q - 1;
+document.getElementById("cakes_14").innerHTML  = q;
+tot = tot - 18;
+}
+
+function sheet_quarter_add() {
+r = r + 1;
+document.getElementById("sheet_quarter").innerHTML  = r;
+tot = tot + 18;
+}
+function sheet_quarter_minus() {
+r = r - 1;
+document.getElementById("sheet_quarter").innerHTML  = r;
+tot = tot - 18;
+}
+
+function sheet_half_add() {
+s = s + 1;
+document.getElementById("sheet_half").innerHTML  = s;
+tot = tot + 18;
+}
+function sheet_half_minus() { 
+s = s - 1;
+document.getElementById("sheet_half").innerHTML  = s;
+tot = tot - 18;
+}
+
+function sheet_full_add() {
+t = t + 1;
+document.getElementById("sheet_full").innerHTML  = t;
+tot = tot + 18;
+}
+function sheet_full_minus() { 
+t = t - 1;
+document.getElementById("sheet_full").innerHTML  = t;
+tot = tot - 18;
+}
+
+function cookie_cake_12_heart_add() {
+u = u + 1;
+document.getElementById("cookie_cake_12_heart").innerHTML  = u;
+tot = tot + 18;
+}
+function cookie_cake_12_heart_minus() { 
+u = u - 1;
+document.getElementById("cookie_cake_12_heart").innerHTML  = u;
+tot = tot - 18;
+}
+
+function cookie_cake_12_round_add() {
+v = v + 1;
+document.getElementById("cookie_cake_12_round").innerHTML  = v;
+tot = tot + 18;
+}
+function cookie_cake_12_round_minus() {
+v = v - 1;
+document.getElementById("cookie_cake_12_round").innerHTML  = v;
+tot = tot - 18;
+}
+
+function cookie_cake_14_round_add() {
+w = w + 1;
+document.getElementById("cookie_cake_14_round").innerHTML  = w;
+tot = tot + 18;
+}
+function cookie_cake_14_round_minus() {
+w = w - 1;
+document.getElementById("cookie_cake_14_round").innerHTML  = w;
+tot = tot - 18;
+}
+
+function cookie_cake_16_round_add() {
+x = x + 1;
+document.getElementById("cookie_cake_16_round").innerHTML  = x;
+tot = tot + 18;
+}
+function cookie_cake_16_round_minus() { 
+x = x - 1;
+document.getElementById("cookie_cake_16_round").innerHTML  = x;
+tot = tot - 18;
+}
+
+function cookie_cake_half_add() {
+y = y + 1;
+document.getElementById("cookie_cake_half").innerHTML  = y;
+tot = tot + 18;
+}
+function cookie_cake_half_minus() { 
+y = y - 1;
+document.getElementById("cookie_cake_half").innerHTML  = y;
+tot = tot - 18;
+}
+
+function cookie_cake_quarter_add() {
+z = z + 1;
+document.getElementById("cookie_cake_quarter").innerHTML  = z;
+tot = tot + 18;
+}
+function cookie_cake_quarter_minus() { 
+z = z - 1;
+document.getElementById("cookie_cake_quarter").innerHTML  = z;
+tot = tot - 18;
+} 
+ 
+
+
+
+function mytotal() {
+
+document.getElementById("here").innerHTML = tot;
+
+}
